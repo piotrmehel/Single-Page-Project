fae0ace52b1b07a91cf6178a94ee52e035d3a5b6
mehelbangin <mehel.bangin@gmail.com>
2021-01-07 11:44:23 +0100

    navbar i intersection-observer

---
+++ observer.js
@@ -0,0 +1,21 @@
+const nav=document.querySelector("nav");
+const sectionOne=document.querySelector("#hero");
+
+const sectionOneOptions={
+    rootMargin:"-100px 0px 0px 0px"
+};
+
+const sectionOneObserver=new IntersectionObserver(function(
+    entries,
+    sectionOneObserver
+){
+    entries.forEach(entry=>{
+        if(!entry.isIntersecting){
+            nav.classList.add("nav-scrolled");
+        }else{
+            nav.classList.remove("nav-scrolled");
+        }
+    });
+},sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
