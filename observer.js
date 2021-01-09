9f2839e8e4cf372f03d48ced07b3ef0f2e2ff180
mehelbangin <mehel.bangin@gmail.com>
2021-01-09 10:36:56 +0100

    sekcja-2: fade-in

--- observer.js
+++ observer.js
@@ -1,5 +1,29 @@
 const nav=document.querySelector("nav");
 const sectionOne=document.querySelector("#hero");
+const faders=document.querySelectorAll(".fade-in");
+
+const appearOptions={
+    threshold:0,
+    rootMargin:"0px 0px -250px 0px"
+};
+
+const appearOnScroll=new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+){
+    entries.forEach(entry=>{
+        if(!entry.isIntersecting){
+            return;
+        }else{
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+},appearOptions);
+
+faders.forEach(fader=>{
+    appearOnScroll.observe(fader);
+});
 
 const sectionOneOptions={
     rootMargin:"-470px 0px 0px 0px"
