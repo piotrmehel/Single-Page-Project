a57e1b0ff228b18a455beee31c88cb562c277265
mehelbangin <mehel.bangin@gmail.com>
2021-01-14 20:01:10 +0100

    elkos

--- observer.js
+++ observer.js
@@ -84,3 +84,35 @@ setTimeout(()=>{
 
 // Get the current year for the copyright
 $('#year').text(new Date().getFullYear());
+
+//smooth scroll
+$(document).ready(function() {
+  
+    var scrollLink = $('.scroll');
+    
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      let nav=$('.navbar').outerHeight();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top - nav -20
+      }, 700 );
+    });
+    
+    // Active link switching
+    $(window).scroll(function() {
+      var scrollbarLocation = $(this).scrollTop();
+      
+      scrollLink.each(function() {
+        
+        var sectionOffset = $(this.hash).offset().top - 20;
+        
+        if ( sectionOffset <= scrollbarLocation ) {
+          $(this).parent().addClass('active');
+          $(this).parent().siblings().removeClass('active');
+        }
+      })
+      
+    })
+    
+  })
