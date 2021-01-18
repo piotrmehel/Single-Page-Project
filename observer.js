f497ee19b87a6f4a889e83e37589859421731fb7
mehelbangin <mehel.bangin@gmail.com>
2021-01-18 11:02:57 +0100

    working forms

--- observer.js
+++ observer.js
@@ -162,3 +162,57 @@ $(document).ready(function() {
 }
 
 navSlide();
+
+//contact submission
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("my-form");
+    var status = document.getElementById("status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      status.classList.add("submited");
+      status.innerHTML = "Dziękujemy za wiadomość!";
+      setTimeout(()=>{
+        status.classList.remove("submited")
+      },5000);
+    }
+
+    function error() {  
+      status.classList.add("errorek");  
+      status.innerHTML = "Błąd w formularzu!";
+      setTimeout(()=>{
+        status.classList.remove("errorek")
+      },5000);
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
