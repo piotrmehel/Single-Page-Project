aead5e9ef81c8486e1415e35223d14018d99e6c6
mehelbangin <mehel.bangin@gmail.com>
2021-01-29 10:27:28 +0100

    icons

--- observer.js
+++ observer.js
@@ -216,3 +216,44 @@ window.addEventListener("DOMContentLoaded", function() {
     };
     xhr.send(data);
   }
+
+  //animated-couter
+  const counters=document.querySelectorAll('.counter');
+  const speed=1000;
+  const animatedOptions={
+    threshold:0,
+    rootMargin:"0px 0px -50px 0px"
+};
+  
+const animatedCounters=new IntersectionObserver(function(
+    entries,
+    animatedCounters
+    ){
+    entries.forEach(entry=>{
+        if(!entry.isIntersecting){
+            return;
+        }else{
+            counters.forEach(counter=>{
+                const updateCount=()=>{
+                    const target=+counter.getAttribute('data-target');
+                    const count=+counter.innerText;
+          
+                    const inc=target/speed;
+          
+                    if(count<target){
+                        counter.innerText=Math.ceil(count+inc);
+                        setTimeout(updateCount,10);
+                    }else{
+                        count.innerText=target;
+                    }
+                }
+                updateCount();
+            });
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+},animatedOptions);
+
+counters.forEach(counter=>{
+    animatedCounters.observe(counter);
+});
